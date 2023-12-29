2b2ae7e49c7e028d095f030c8ea16bb6e05fc1ad
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-12-29 16:09:18 +0100

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,29 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+
+  ],
+  theme: {
+    colors: {
+      'app': {
+        50: '#f3f1ff',
+        100: '#ebe5ff',
+        200: '#d9ceff',
+        300: '#bea6ff',
+        400: '#9f75ff',
+        500: '#843dff',
+        600: '#7916ff',
+        700: '#6b04fd',
+        800: '#5a03d5',
+        900: '#4b05ad',
+        950: '#2c0076'
+      }
+    },
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
